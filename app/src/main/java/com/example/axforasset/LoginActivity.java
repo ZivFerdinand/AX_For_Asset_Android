b86e530e1c7cbbbed519804b3076fc3584a82919
tristan <tristanaaron.2711@gmail.com>
2024-07-22 13:37:22 +0700

    Done Simple Navigating
    @ZivFerdinand

--- app/src/main/java/com/example/axforasset/LoginActivity.java
+++ app/src/main/java/com/example/axforasset/LoginActivity.java
@@ -2,13 +2,26 @@ package com.example.axforasset;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(
+                v -> {
+                    // Intent untuk berpindah ke LoginActivity
+                    Intent intent = new Intent(LoginActivity.this, ProfileActivity.class);
+                    startActivity(intent);
+                }
+        );
     }
+
 }
