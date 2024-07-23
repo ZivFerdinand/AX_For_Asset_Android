8d9a3ddb29b1b073b0c12d1b6c04bae71dd33d33
gladysruslim <gladysruslim22@gmail.com>
2024-07-23 11:23:28 +0700

    Done Image Carrousel Creating
    @ZivFerdinand

---
+++ app/src/main/java/com/example/axforasset/Home.java
@@ -0,0 +1,79 @@
+package com.example.axforasset;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Home extends AppCompatActivity {
+
+    ViewPager2 viewPager2;
+    private Handler slideHandler = new Handler();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        viewPager2 = findViewById(R.id.viewPager);
+
+        List<SlideItem> sliderItem = new ArrayList<>();
+        sliderItem.add(new SlideItem(R.drawable.profile_image));
+        sliderItem.add(new SlideItem(R.drawable.cat_login));
+        sliderItem.add(new SlideItem(R.drawable.logo));
+        sliderItem.add(new SlideItem(R.drawable.logo_full));
+
+        viewPager2.setAdapter(new SlideAdapter(sliderItem, viewPager2));
+        viewPager2.setClipToPadding(false);
+        viewPager2.setClipChildren(false);
+        viewPager2.setOffscreenPageLimit(5);
+        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+
+        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
+        compositePageTransformer.addTransformer(new MarginPageTransformer(30));
+        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                float r = 1-Math.abs(position);
+                page.setScaleY(0.85f + r*0.15f  );
+            }
+        });
+        viewPager2.setPageTransformer(compositePageTransformer);
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+
+                slideHandler.removeCallbacks(sliderRunnable);
+                slideHandler.postDelayed(sliderRunnable, 3000);
+            }
+        });
+    }
+
+    private Runnable sliderRunnable = new Runnable() {
+        @Override
+        public void run() {
+            viewPager2.setCurrentItem(viewPager2.getCurrentItem() + 1);
+        }
+    };
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        slideHandler.removeCallbacks(sliderRunnable);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        slideHandler.postDelayed(sliderRunnable, 3000);
+    }
+}
---
+++ app/src/main/java/com/example/axforasset/SlideItem.java
@@ -0,0 +1,15 @@
+package com.example.axforasset;
+
+public class SlideItem {
+    private int image;
+
+    SlideItem(int image)
+    {
+        this.image = image;
+    }
+
+    public int getImage()
+    {
+        return image;
+    }
+}
